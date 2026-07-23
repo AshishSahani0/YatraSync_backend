1c9d6307b9a12584a3c462f2040322152e66e418
Ashish Sahani <ashishsahani0011@gmail.com>
2026-07-23 14:58:55 +0530

    chore: allow google fonts in CSP to avoid console errors

--- src/main/java/com/example/backend/auth/config/SecurityConfig.java
+++ src/main/java/com/example/backend/auth/config/SecurityConfig.java
@@ -53,7 +53,8 @@ public class SecurityConfig {
     csp.policyDirectives(
         "default-src 'self'; " +
         "script-src 'self' https://accounts.google.com; " +
-        "style-src 'self' 'unsafe-inline'; " +
+        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+        "font-src 'self' https://fonts.gstatic.com; " +
         "img-src 'self' data: https://accounts.google.com; " +
         "frame-src https://accounts.google.com; " +
         "connect-src 'self' http://localhost:3000 https://yatrasync.netlify.app https://yatrasync-backend.onrender.com"
